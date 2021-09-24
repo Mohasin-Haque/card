1fd042038a53d42973dbf10d62436ca6b418bcb8
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-09-24 20:51:48 +0530

    added link

---
+++ script.js
@@ -0,0 +1,7 @@
+const Btn = document.querySelector(".btn");
+
+function followMe(){
+    window.location = "https://www.instagram.com/its_mohasin_official/";
+}
+
+Btn.addEventListener("click", followMe )
